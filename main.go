66ce311876b2bd8767de6cbe7f7d7328655647c5
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-04-15 20:31:37 -0400

    completed

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	// window dimensions
 	const (
 		screenWidth  = 800
 		screenHeight = 450
@@ -12,11 +13,70 @@ func main() {
 	rl.InitWindow(screenWidth, screenHeight, "Axe Game Go")
 	defer rl.CloseWindow()
 
+	// circle coordinates
+	circleX := 200
+	circleY := 200
+	circleRadius := 25
+	// circle edges
+	lCircleX := circleX - circleRadius
+	rCircleX := circleX + circleRadius
+	uCircleY := circleY - circleRadius
+	bCircleY := circleY + circleRadius
+
+	// axe coordinates
+	axeX := 400
+	axeY := 0
+	axeLength := 50
+	// axe edges
+	lAxeX := axeX
+	rAxeX := axeX + axeLength
+	uAxeY := axeY
+	bAxeY := axeY + axeLength
+
+	direction := 10
+
+	collisionWithAxe := (bAxeY >= uCircleY) && (uAxeY <= bCircleY) && (lAxeX <= rCircleX) && (rAxeX >= lCircleX)
+
 	rl.SetTargetFPS(60)
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Game Over!", 400, 200, 20, rl.Red)
+
+		if collisionWithAxe {
+			rl.DrawText("Game Over!", 400, 200, 20, rl.Red)
+		} else {
+			// game logic begin
+
+			// update the edges
+			lCircleX = circleX - circleRadius
+			rCircleX = circleX + circleRadius
+			uCircleY = circleY - circleRadius
+			bCircleY = circleY + circleRadius
+
+			lAxeX = axeX
+			rAxeX = axeX + axeLength
+			uAxeY = axeY
+			bAxeY = axeY + axeLength
+
+			collisionWithAxe = (bAxeY >= uCircleY) && (uAxeY <= bCircleY) && (lAxeX <= rCircleX) && (rAxeX >= lCircleX)
+
+			rl.DrawCircle(int32(circleX), int32(circleY), float32(circleRadius), rl.Blue)
+			rl.DrawRectangle(int32(axeX), int32(axeY), int32(axeLength), int32(axeLength), rl.Red)
+
+			// move the axe
+			axeY += direction
+			if axeY > screenHeight || axeY < 0 {
+				direction = -direction
+			}
+
+			if rl.IsKeyDown(rl.KeyD) && circleX < screenWidth {
+				circleX += 10
+			}
+			if rl.IsKeyDown(rl.KeyA) && circleX > 25 {
+				circleX -= 10
+			}
+
+		}
 
 		rl.EndDrawing()
 
